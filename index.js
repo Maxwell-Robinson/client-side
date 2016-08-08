a579adc82082f7044701862f6f529893c63bab6e
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-09 11:26:22 +1200

    Updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-  var issData = data.body
+  var issData = JSON.Parse(data.body)
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Max', issData: issData})
