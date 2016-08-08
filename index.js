3c5a8625aad741e2a3090f42954ff90343ec972e
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-09 11:56:41 +1200

    Updates

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ xhr.get(endpoint, function (err, data) {
   }
 
   // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-  var issData = data.body
+  console.log(JSON.parse(data.body)) // FYI: data.body is a string
+  var data = JSON.parse(data.body)
+  var issAlt = data.altitude
+  var issLat = data.latitude
+  var issLong = data.longitude
+  var issTime = data.timestamp
+  var issName =  data.name.toUpperCase()
+  var issSpeed = data.velocity
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Max', issData: issData})
+  target.innerHTML = greeting({name: 'Max', issAlt: issAlt, issLat: issLat, issLong: issLong, issTime: issTime, issName: issName, issSpeed: issSpeed })
 })
