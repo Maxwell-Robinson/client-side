5207c8961e4800f315c49470b7318b5d9c62af82
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-09 11:09:40 +1200

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
+
+
   }
 
   // In case you're curious
